9f1f5c71c9f9da7ddedd0978f62111c2904a3286
rkaaya <gergelynagy99@gmail.com>
2024-07-10 21:29:01 +0200

    Init

---
+++ src/test/java/com/rkaaya/javaspringbestpractises/JavaSpringBestPractisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkaaya.javaspringbestpractises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBestPractisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
