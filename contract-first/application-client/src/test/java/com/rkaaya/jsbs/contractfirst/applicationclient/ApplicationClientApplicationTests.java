f6fca802e24db3fb1ef86b3caae85c9015287c41
rkaaya <gergelynagy99@gmail.com>
2024-07-10 22:02:07 +0200

    Init structure

---
+++ contract-first/application-client/src/test/java/com/rkaaya/jsbs/contractfirst/applicationclient/ApplicationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkaaya.jsbs.contractfirst.applicationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ contract-first/src/test/java/com/rkaaya/jsbs/contractfirst/ContractFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkaaya.jsbs.contractfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
